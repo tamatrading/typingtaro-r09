a3f9affb312b10f60e617bfe23b450f7b9b423a3
tamatrading <git@stackblitz.com>
2024-12-07 13:58:20 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typingTaro-r09
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/tamatrading/typingtaro-r09)
---
+++ src/types/game.ts
@@ -0,0 +1,30 @@
+export interface GameSettings {
+  selectedStages: number[];
+  speed: number;
+  isRandomMode: boolean;
+  numStages: number;
+  showHands: boolean;
+}
+
+export interface ScorePopup {
+  id: number;
+  score: number;
+  x: number;
+  y: number;
+}
+
+export interface CurrentWord {
+  id: number;
+  text: string;
+  x: number;
+  y: number;
+  speed: number;
+  startTime: number;
+}
+
+export interface Particle {
+  id: number;
+  x: number;
+  y: number;
+  color: string;
+}
